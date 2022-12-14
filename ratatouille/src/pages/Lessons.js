f801b4d132b3d35b6982e140fe7569eb02965001
chloeho7 <chloeho7@gmail.com>
2022-12-14 17:54:49 -0500

    added nav bar to lessons and search

--- ratatouille/src/pages/Lessons.js
+++ ratatouille/src/pages/Lessons.js
@@ -1,9 +1,29 @@
+
+import '../App.css';
+import {useNavigate} from 'react-router-dom';
+import Logo from '../Logo_orange.png';
+
 function Lessons(){
-    return(
-        <div className = 'page'>
-            <h1> Lessons </h1>
+    let navigate = useNavigate();
+    return (
+    <div class="row">
+      <div class="column left">
+        <img src={Logo} class='smallLogo'></img>
+        <div class="container">
+          <i class="fa fa-solid fa-user user" style={{ fontSize: '2em' }} />
+          <div class="btn-group">
+            <button onClick={() => {navigate("/login-landing")}}> Login </button>
+            <button onClick={() => {navigate("/")}}>Home</button>
+            <button onClick={() => {navigate("/profile");}}>Profile</button>
+            <button onClick={() => {navigate("/lessons");}}>Lessons</button>
+            <button onClick={() => {navigate("/ratatouillify");}}>Ratatouillify</button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+    //<div class="column right">
+   // </div>
+  );
 }
 
 export default Lessons;
