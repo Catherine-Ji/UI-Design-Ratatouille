f2c58983676fe4feb1ac73a13493fb4ba0b9b488
margotstern <margot.stern02@gmail.com>
2022-12-15 09:54:28 -0500

    added side nav

--- ratatouille/src/pages/Ratatouillify.js
+++ ratatouille/src/pages/Ratatouillify.js
@@ -1,8 +1,31 @@
+import '../App.css';
+import {useNavigate} from 'react-router-dom';
+import Logo from '../Logo_orange.png';
+
 function Ratatouillify(){
+    let navigate = useNavigate();
     return(
-        <div className = 'page'>
-            <h1> Ratatouillify </h1>
+     <div class="row">
+      <div class="column left">
+        <img src={Logo} class='smallLogo'></img>
+        <div class="container">
+          <i class="fa fa-solid fa-user user" style={{ fontSize: '2em' }} />
+          <div class="btn-group">
+            <button onClick={() => {navigate("/login-landing")}}> Login </button>
+            <button onClick={() => {navigate("/")}}>Home</button>
+            <button onClick={() => {navigate("/profile");}}>Profile</button>
+            <button onClick={() => {navigate("/lessons");}}>Lessons</button>
+            <button onClick={() => {navigate("/ratatouillify");}}>Ratatouillify</button>
+          </div>
         </div>
+      </div>
+
+      
+              <div className = 'page'>
+            <h1> Ratatouillifyy </h1>
+        </div>
+    </div>     
+
     )
 }
 
