803dfd388f60c681b8c7a86e02b03cd0f19e2f24
Catherine Ji <catherineji415@gmail.com>
2022-12-16 15:29:54 -0500

    pushing my local version

--- ratatouille/src/pages/LoginLanding.js
+++ ratatouille/src/pages/LoginLanding.js
@@ -6,7 +6,7 @@ export default function Login_landing(){
     let navigate = useNavigate();
     return (
         <div className = 'loginpage'>
-            <img src={Logo} class='extraLargeLogo_landing'></img>
+            <img src={Logo} className='extraLargeLogo_landing'></img>
             <h1 className='headertext_white loginlandingtext'> Let's get cookin' </h1>
             <button className='loginbtn_white' onClick={() => navigate("/login")}>Login</button>
             <button className='loginbtn_grey' onClick={()=> navigate("/create-account")}>Create Account</button>
