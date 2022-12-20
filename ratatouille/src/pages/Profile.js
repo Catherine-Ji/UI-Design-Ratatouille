8f7e459dea7832042c5625917c74315975561bb0
Catherine Ji <catherineji415@gmail.com>
2022-12-20 17:23:36 -0500

    added nav bar to profile page

--- ratatouille/src/pages/Profile.js
+++ ratatouille/src/pages/Profile.js
@@ -1,9 +1,29 @@
+import '../App.css';
+import {useNavigate} from 'react-router-dom';
+import Logo from '../Logo_orange.png';
+
 function Profile(){
-    return(
-        <div className = 'page'>
-            <h1> Profile </h1>
+    let navigate = useNavigate();
+    return (
+        <div class="row">
+          <div class="column left">
+            <img src={Logo} class='smallLogo'></img>
+            <div class="container">
+              <i class="fa fa-solid fa-user user" style={{ fontSize: '2em', color: 'var(--grey)' }} />
+              <div class="btn-group">
+                <button onClick={() => {navigate("/login-landing")}}> Login </button>
+                <button onClick={() => {navigate("/")}}>Home</button>
+                <button onClick={() => {navigate("/profile");}}>Profile</button>
+                <button onClick={() => {navigate("/lessons");}}>Lessons</button>
+                <button onClick={() => {navigate("/ratatouillify");}}>Ratatouillify</button>
+              </div>
+            </div>
+          </div>
+          <div class="column right">
+            <h1> Profile Page </h1>
+          </div>
         </div>
-    )
+      );
 }
 
 export default Profile;
